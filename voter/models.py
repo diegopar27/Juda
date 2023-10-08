0dd4082ea621cf3caf6db49bb7d5a817b540b1c7
diegopar27 <102553545+diegopar27@users.noreply.github.com>
2023-10-08 11:56:53 -0500

    eliminar lineas

--- voter/models.py
+++ voter/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-
-# Create your models here.
-
 TYPE = (
     ('cedula', 'Cedula'),
     ('tarjeta de identidad', 'Tarjeta de identidad'),
--- voter/views.py
+++ voter/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import viewsets
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import status
+
 from .models import Voter
 from .serializers import VoterSerializer
 
